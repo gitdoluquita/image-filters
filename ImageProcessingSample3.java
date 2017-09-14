ef32e3f112e73e86cc56c72930f1a9d9d738f4f6
Luccas Quadros <lucc_70@hotmail.com>
2017-09-14 18:34:09 -0300

    Algoritmo oil painting adicionado mas não funcionando

--- ImageProcessingSample3.java
+++ ImageProcessingSample3.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.swing.*;
 import java.awt.GridLayout;
 
+
 public class ImageProcessingSample3 {
     public static void main (String[] args) {
         BufferedImage originalImage;
@@ -14,7 +15,7 @@ public class ImageProcessingSample3 {
             JOptionPane.showMessageDialog(null, "Error: " + ex.toString());
             return;
         }
-        
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(2, 2));
@@ -24,6 +25,7 @@ public class ImageProcessingSample3 {
             convertToGrayScale(originalImage),
             invertImageColors(convertToGrayScale(originalImage)),
             blurImage(originalImage, 10)
+            //OilPainting(originalImage,1,200)
         };
         for (BufferedImage image : images) {
             JLabel imageLabel = new JLabel(new ImageIcon(image));
@@ -81,7 +83,7 @@ public class ImageProcessingSample3 {
                 // int pixelValue = (255 << 16) + // red
                 //     (255 << 8) + // green
                 //     (255) - // blue
-                //     originalImage.getRGB(x, y); 
+                //     originalImage.getRGB(x, y);
 
                 invertedImage.setRGB(x, y, pixelValue);
             }
@@ -109,7 +111,7 @@ private static BufferedImage blurImage(
 
             for(int xNeighbor = x - level;
                     xNeighbor <= x + level;
-                    ++xNeighbor) {                    
+                    ++xNeighbor) {
                 for(int yNeighbor = y - level;
                         yNeighbor <= y + level;
                         ++yNeighbor) {
@@ -131,4 +133,54 @@ private static BufferedImage blurImage(
 
     return blurredImage;
 }
+
+private static BufferedImage OilPainting(BufferedImage originalImage, int radius, int level){
+  int columns = originalImage.getWidth();
+  int rows = originalImage.getHeight();
+  BufferedImage OilImage = new BufferedImage(columns, rows, BufferedImage.TYPE_INT_RGB);
+
+  for(int x = 0; x < columns; ++x) {
+      for(int y = 0; y < rows; ++y) {
+        int[] intensityCount =new int[level];
+        int[] averageR =new int[level];
+        int[] averageG =new int[level];
+        int[] averageB =new int[level];
+        int pixelValue = originalImage.getRGB(x, y);
+        int red = 255 - pixelValue >> 16 & 0xFF;
+        int green = 255 - pixelValue >> 8 & 0xFF;
+        int blue = 255 - pixelValue & 0xFF;
+        int maxIndex = 0;
+        int curMax = -1;
+
+        for (int i = y-radius; i < y+radius; i++) {
+          for (int j = x; (j-x)*(j-x) + (i-y)*(i-y) <= radius*radius; j--) {
+              int curIntensity = (int)((red+green+blue)/3*level)/255;
+              intensityCount[curIntensity]++;
+              averageR[curIntensity] += red;
+              averageG[curIntensity] += green;
+              averageB[curIntensity] += blue;
+          }
+          for (int j = x+1; (j-x)*(j-x) + (i-y)*(i-y) <= radius*radius; j++) {
+              int curIntensity = (int)((red+green+blue)/3*level)/255;
+              intensityCount[curIntensity]++;
+              averageR[curIntensity] += red;
+              averageG[curIntensity] += green;
+              averageB[curIntensity] += blue;
+          }
+        }
+        for (int i=0;i<level;i++){
+          if (intensityCount[i] > curMax){
+            curMax= intensityCount[i];
+            maxIndex= i;
+          }
+        }
+        red = averageR[maxIndex] / curMax;
+        green = averageG[maxIndex] / curMax;
+        blue = averageB[maxIndex] / curMax;
+        pixelValue = (red << 16) + (green << 8) + blue;
+        OilImage.setRGB(x, y, pixelValue);
+      }
+    }
+    return OilImage;
+  }
 }
